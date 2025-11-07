9ddb9dfbc5c9b8ff3993b30a3dc3ac377fe75a0d
Aditya joshi <168006690+Aditya44234@users.noreply.github.com>
2025-11-07 19:22:07 +0530

    remove API

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -3,21 +3,7 @@ const products = require('../data/products');
 
 const getProducts = async (req, res, next) => {
     try {
-        if (process.env.NODE_ENV === 'production') {
-            // Use mock products in production
-            res.json(products);
-        } else {
-            // Use Fake Store API in development
-            const response = await axios.get("https://fakestoreapi.com/products?limit=8");
-            const apiProducts = response.data.map(item => ({
-                productId: item.id,
-                name: item.title,
-                price: item.price,
-                image: item.image,
-                tag: "StoreAPI"
-            }));
-            res.json(apiProducts);
-        }
+        res.json(products);
     } catch (error) {
         next(error);
     }
